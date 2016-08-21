e6e0ed3ef098eb4b13f77128d8de27cb9bbd38ca
Carson McManus <tascardude@gmail.com>
2016-08-21 11:29:27 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Text;
+using System.Linq;
+
+/// <summary>
+///   The main entry point for the application
+/// </summary>
+[STAThread]
+public static void Main(string[] args)
+{
+    
+}
