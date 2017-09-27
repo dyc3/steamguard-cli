5c7f31dc96da1aaeb06ffecb9da36c5793942d39
Carson McManus <carson.mcmanus1@gmail.com>
2017-09-27 00:39:10 -0400

    increment version number

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.3.3.0")]
-[assembly: AssemblyFileVersion("0.3.3.0")]
+[assembly: AssemblyVersion("0.3.3.1")]
+[assembly: AssemblyFileVersion("0.3.3.1")]
