bde8a37d3f07e868e3d37a754eee8ae88368aa0a
Carson McManus <carson.mcmanus1@gmail.com>
2021-03-27 09:31:38 -0400

    add argument parsing

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,54 @@ use steamguard_cli::*;
 use ::std::*;
 use text_io::read;
 use std::path::Path;
+use clap::{App, Arg, crate_version};
 
 mod steamapi;
 mod accountmanager;
 
 fn main() {
+	let matches = App::new("steamguard-cli")
+		.version(crate_version!())
+		.bin_name("steamguard")
+		.author("dyc3 (Carson McManus)")
+		.about("Generate Steam 2FA codes and confirm Steam trades from the command line.")
+		.arg(
+			Arg::with_name("username")
+				.long("username")
+				.short("u")
+				.help("Select the account you want by steam username. By default, the first account in the manifest is selected.")
+		)
+		.arg(
+			Arg::with_name("all")
+				.long("all")
+				.short("a")
+				.help("Select all accounts in the manifest.")
+		)
+		.arg(
+			Arg::with_name("mafiles-path")
+				.long("mafiles-path")
+				.short("m")
+				.default_value("~/maFiles")
+				.help("Specify which folder your maFiles are in.")
+		)
+		.arg(
+			Arg::with_name("passkey")
+				.long("passkey")
+				.short("p")
+				.help("Specify your encryption passkey.")
+		)
+		.subcommand(
+			App::new("trade")
+				.about("Interactive interface for trade confirmations")
+				.arg(
+					Arg::with_name("accept-all")
+					.short("a")
+					.long("accept-all")
+					.help("Accept all open trade confirmations. Does not open interactive interface.")
+				)
+		)
+		.get_matches();
+
 	println!("Hello, world!");
 
 	// let server_time = steamapi::get_server_time();
