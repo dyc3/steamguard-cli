5a004efd06ff90742d85ad32d58820f39aa9e9d3
Carson McManus <carson.mcmanus1@gmail.com>
2021-08-20 12:17:59 -0400

    update package metadata and stuff

--- package.sh
+++ package.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-VERSION=$(build/steamguard --help | head -n 1 | cut -d v -f 2)"-0"
+BIN_PATH="target/steamguard-cli"
+if [[ ! -f "${BIN_PATH}" ]]; then
+	echo "ERROR: Could not find release binaries, building them..."
+	cargo build --release
+fi
+VERSION=$("$BIN_PATH" --version | cut -d " " -f 2)"-0"
 TEMP_PKG_PATH="/tmp/steamguard-cli_$VERSION"
-echo Building Debian package for v$VERSION...
+echo "Building Debian package for v$VERSION..."
 
-mkdir -p $TEMP_PKG_PATH/usr/local/bin
-mkdir -p $TEMP_PKG_PATH/etc/bash_completion.d
-mkdir -p $TEMP_PKG_PATH/DEBIAN
+mkdir -p "$TEMP_PKG_PATH/usr/local/bin"
+mkdir -p "$TEMP_PKG_PATH/etc/bash_completion.d"
+mkdir -p "$TEMP_PKG_PATH/DEBIAN"
 
-cp build/* $TEMP_PKG_PATH/usr/local/bin
-cp bash-tab-completion $TEMP_PKG_PATH/etc/bash_completion.d/steamguard
+cp "$BIN_PATH" "$TEMP_PKG_PATH/usr/local/bin/steamguard"
+"$BIN_PATH" completion --shell bash > "$TEMP_PKG_PATH/etc/bash_completion.d/steamguard"
 
 cat <<EOT >> $TEMP_PKG_PATH/DEBIAN/control
 Package: steamguard-cli
-Depends: mono-complete
+Depends:
 Version: $VERSION
 Section: base
 Priority: optional
 Architecture: all
-Maintainer: Carson McManus <dyc3@users.noreply.github.com>
+Maintainer: Carson McManus <carson.mcmanus1@gmail.com>
 Description: steamguard-cli
- A command line utility to generate Steam Guard codes
- (AKA 2 factor authentication codes).
+ A command line utility to generate Steam 2FA codes and respond to confirmations.
 EOT
 
-dpkg-deb --build $TEMP_PKG_PATH steamguard-cli_$VERSION.deb
+dpkg-deb --build "$TEMP_PKG_PATH" "steamguard-cli_$VERSION.deb"
 
-rm -rf $TEMP_PKG_PATH
+rm -rf "$TEMP_PKG_PATH"
