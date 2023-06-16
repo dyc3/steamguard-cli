e1c8c49727c60ec5cb30cbdd040147fb6c3a52aa
Carson McManus <carson.mcmanus1@gmail.com>
2023-06-16 12:57:09 -0400

    add serde stuff to protobuf generated code

--- steamguard/build.rs
+++ steamguard/build.rs
@@ -1,7 +1,12 @@
 use std::path::Path;
 use std::path::PathBuf;
 
+use protobuf::descriptor::field_descriptor_proto::Type;
+use protobuf::reflect::FieldDescriptor;
+use protobuf::reflect::MessageDescriptor;
 use protobuf_codegen::Codegen;
+use protobuf_codegen::Customize;
+use protobuf_codegen::CustomizeCallback;
 
 fn main() {
 	// let current_dir = std::env::current_dir().unwrap();
@@ -16,7 +21,7 @@ fn main() {
 	}
 	codegen.cargo_out_dir("protobufs");
 
-	codegen.run().expect("protobuf codegen failed");
+	codegen.customize_callback(GenSerde).run_from_script();
 	println!("cargo:rerun-if-changed=protobufs");
 	println!("cargo:rerun-if-changed=build.rs");
 }
@@ -34,4 +39,26 @@ fn get_all_proto_paths<P: AsRef<Path>>(dir: P) -> anyhow::Result<Vec<PathBuf>> {
 		}
 	}
 	Ok(paths)
+}
+
+struct GenSerde;
+
+impl CustomizeCallback for GenSerde {
+	fn message(&self, _message: &MessageDescriptor) -> Customize {
+		Customize::default().before("#[derive(::serde::Serialize, ::serde::Deserialize)]")
+	}
+
+	fn field(&self, field: &FieldDescriptor) -> Customize {
+		if field.proto().type_() == Type::TYPE_ENUM {
+			// `EnumOrUnknown` is not a part of rust-protobuf, so external serializer is needed.
+			Customize::default().before(
+				"#[serde(serialize_with = \"crate::protobufs::serialize_enum_or_unknown\", deserialize_with = \"crate::protobufs::deserialize_enum_or_unknown\")]")
+		} else {
+			Customize::default()
+		}
+	}
+
+	fn special_field(&self, _message: &MessageDescriptor, _field: &str) -> Customize {
+		Customize::default().before("#[serde(skip)]")
+	}
 }
--- steamguard/src/protobufs.rs
+++ steamguard/src/protobufs.rs
@@ -1,1 +1,64 @@
+use std::fmt::Formatter;
+use std::marker::PhantomData;
+
+use protobuf::EnumFull;
+use protobuf::EnumOrUnknown;
+
 include!(concat!(env!("OUT_DIR"), "/protobufs/mod.rs"));
+
+fn serialize_enum_or_unknown<E: EnumFull, S: serde::Serializer>(
+	e: &Option<EnumOrUnknown<E>>,
+	s: S,
+) -> Result<S::Ok, S::Error> {
+	if let Some(e) = e {
+		match e.enum_value() {
+			Ok(v) => s.serialize_str(v.descriptor().name()),
+			Err(v) => s.serialize_i32(v),
+		}
+	} else {
+		s.serialize_unit()
+	}
+}
+
+fn deserialize_enum_or_unknown<'de, E: EnumFull, D: serde::Deserializer<'de>>(
+	d: D,
+) -> Result<Option<EnumOrUnknown<E>>, D::Error> {
+	struct DeserializeEnumVisitor<E: EnumFull>(PhantomData<E>);
+
+	impl<'de, E: EnumFull> serde::de::Visitor<'de> for DeserializeEnumVisitor<E> {
+		type Value = Option<EnumOrUnknown<E>>;
+
+		fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
+			write!(formatter, "a string, an integer or none")
+		}
+
+		fn visit_str<R>(self, v: &str) -> Result<Self::Value, R>
+		where
+			R: serde::de::Error,
+		{
+			match E::enum_descriptor().value_by_name(v) {
+				Some(v) => Ok(Some(EnumOrUnknown::from_i32(v.value()))),
+				None => Err(serde::de::Error::custom(format!(
+					"unknown enum value: {}",
+					v
+				))),
+			}
+		}
+
+		fn visit_i32<R>(self, v: i32) -> Result<Self::Value, R>
+		where
+			R: serde::de::Error,
+		{
+			Ok(Some(EnumOrUnknown::from_i32(v)))
+		}
+
+		fn visit_unit<R>(self) -> Result<Self::Value, R>
+		where
+			R: serde::de::Error,
+		{
+			Ok(None)
+		}
+	}
+
+	d.deserialize_any(DeserializeEnumVisitor(PhantomData))
+}
