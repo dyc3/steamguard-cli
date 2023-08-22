228312b5851e605c02a38d75e25d003bf1cad8bc
Carson McManus <carson.mcmanus1@gmail.com>
2023-08-22 16:17:19 -0400

    import: remove the `--sda` flag, the format should be detected automatically

--- src/commands/import.rs
+++ src/commands/import.rs
@@ -11,9 +11,6 @@ use super::*;
 	about = "Import an account with steamguard already set up. It must not be encrypted. If you haven't used steamguard-cli before, you probably don't need to use this command."
 )]
 pub struct ImportCommand {
-	#[clap(long, help = "Whether or not the provided maFiles are from SDA.")]
-	pub sda: bool,
-
 	#[clap(long, help = "Paths to one or more maFiles, eg. \"./gaben.maFile\"")]
 	pub files: Vec<String>,
 }
@@ -25,27 +22,33 @@ where
 	fn execute(&self, _transport: T, manager: &mut AccountManager) -> anyhow::Result<()> {
 		for file_path in self.files.iter() {
 			debug!("loading entry: {:?}", file_path);
-			if self.sda {
-				let path = Path::new(&file_path);
-				let account =
-					crate::accountmanager::migrate::load_and_upgrade_external_account(path)?;
-				manager.add_account(account);
-				info!("Imported account: {}", &file_path);
-			} else {
-				match manager.import_account(file_path) {
-					Ok(_) => {
-						info!("Imported account: {}", &file_path);
-					}
-					Err(ManifestAccountImportError::AlreadyExists { .. }) => {
-						warn!("Account already exists: {} -- Ignoring", &file_path);
-					}
-					Err(ManifestAccountImportError::DeserializationFailed(err)) => {
-						warn!("Failed to import account: {} {}", &file_path, err);
-						warn!("If this file came from SDA, try using --sda");
-					}
-					Err(err) => {
-						bail!("Failed to import account: {} {}", &file_path, err);
-					}
+			match manager.import_account(file_path) {
+				Ok(_) => {
+					info!("Imported account: {}", &file_path);
+				}
+				Err(ManifestAccountImportError::AlreadyExists { .. }) => {
+					warn!("Account already exists: {} -- Ignoring", &file_path);
+				}
+				Err(ManifestAccountImportError::DeserializationFailed(orig_err)) => {
+					debug!("Falling back to external account import",);
+
+					let path = Path::new(&file_path);
+					let account =
+						match crate::accountmanager::migrate::load_and_upgrade_external_account(
+							path,
+						) {
+							Ok(account) => account,
+							Err(err) => {
+								error!("Failed to import account: {} {}", &file_path, err);
+								error!("The original error was: {}", orig_err);
+								continue;
+							}
+						};
+					manager.add_account(account);
+					info!("Imported account: {}", &file_path);
+				}
+				Err(err) => {
+					bail!("Failed to import account: {} {}", &file_path, err);
 				}
 			}
 		}
